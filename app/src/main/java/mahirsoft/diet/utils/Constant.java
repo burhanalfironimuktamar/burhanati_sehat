a3222780b4d8d1feb5e4ea237a14896d76716b46
atiatulmaulablog <atiatulmaula2@gmail.com>
2016-02-16 22:16:32 +0700

    add

--- app/src/main/java/mahirsoft/diet/utils/Constant.java
+++ app/src/main/java/mahirsoft/diet/utils/Constant.java
@@ -5,7 +5,5 @@ package mahirsoft.diet.utils;
  */
 public class Constant {
     public static final String PREF_NAME = "cache_diet_sehat";
-    public static final String PREF_NAME = "";
-    public static final String PREF_BO = "";
 
 }
