22c6e7763ab8c2a05374ac148a5daa982d1be038
alfironi <burhan@inovidea.net>
2016-02-20 02:11:10 +0700

    GUIDE

--- app/src/main/java/mahirsoft/diet/activity/PanduanActivity.java
+++ app/src/main/java/mahirsoft/diet/activity/PanduanActivity.java
@@ -1,12 +1,17 @@
 package mahirsoft.diet.activity;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import mahirsoft.diet.R;
+import mahirsoft.diet.adapter.GuideAdapter;
 
 public class PanduanActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private String[] data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,10 @@ public class PanduanActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        data = getResources().getStringArray(R.array.guide);
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(new GuideAdapter(this, data));
     }
 
 }
