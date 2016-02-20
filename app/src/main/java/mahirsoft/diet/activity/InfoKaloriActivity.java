c4a2e354f05151de030c1e69eb65c6d575028ed7
atiatulmaulablog <atiatulmaula2@gmail.com>
2016-02-20 11:08:25 +0700

    info kalori

--- app/src/main/java/mahirsoft/diet/activity/InfoKaloriActivity.java
+++ app/src/main/java/mahirsoft/diet/activity/InfoKaloriActivity.java
@@ -1,30 +1,71 @@
 package mahirsoft.diet.activity;
 
+import android.database.Cursor;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.TextView;
 
 import mahirsoft.diet.R;
+import mahirsoft.diet.data.Food;
 
-public class InfoKaloriActivity extends AppCompatActivity {
+public class InfoKaloriActivity extends AppCompatActivity implements View.OnClickListener{
 
     private TextView txtTitle;
+    private Button btnLihat;
+    private AutoCompleteTextView autoComplete;
+    private ArrayAdapter<String> autoCompleteAdapter;
+    private TextView nama;
+    private TextView kalori;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_kalori);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         initView();
     }
 
     private void initView() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        btnLihat = (Button) findViewById(R.id.btn_lihat);
         txtTitle = (TextView) findViewById(R.id.title_info_kalori);
+        autoComplete = (AutoCompleteTextView) findViewById(R.id.autoComplete);
+        nama = (TextView) findViewById(R.id.txtNama);
+        kalori = (TextView) findViewById(R.id.txtJmlKalori);
+
+        Cursor foodCursor = getContentResolver().query(Food.CONTENT_URI, new String[]{Food.COLUMN_NAME}, null, null, null);
+        String[] foods = new String[foodCursor.getCount()];
+        int i = 0;
+        while (foodCursor.moveToNext()){
+            foods[i] = foodCursor.getString(foodCursor.getColumnIndexOrThrow(Food.COLUMN_NAME));
+            i++;
+        }
+        foodCursor.close();
+        autoCompleteAdapter = new ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, foods);
+        autoComplete.setAdapter(autoCompleteAdapter);
         txtTitle.setPaintFlags(txtTitle.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+        btnLihat.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v == btnLihat){
+            String food = autoComplete.getText().toString();
+            Cursor cursor = getContentResolver().query(Food.CONTENT_URI, Food.COLUMNS, Food.COLUMN_NAME+"='"+food+"'", null, null);
+            while (cursor.moveToNext()){
+                nama.setText(cursor.getString(cursor.getColumnIndexOrThrow(Food.COLUMN_NAME)));
+                kalori.setText(cursor.getString(cursor.getColumnIndexOrThrow(Food.COLUMN_KALORI)));
+            }
+            cursor.close();
+        }
+    }
 }
